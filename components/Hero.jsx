c0870480222dce80738ea26f8419b8e6d6cc8da0
ohunyanC <wildcardcharly@outlook.com>
2022-11-02 19:20:37 +0100

    commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Hero = ({ heading, message }) => {
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-auto custom-img">
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover bg-no-repeat custom-img">
       {/*overlay*/}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] mt-[-10rem]">
